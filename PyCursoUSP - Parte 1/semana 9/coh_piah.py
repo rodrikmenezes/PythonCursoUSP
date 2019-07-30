97290dfde3fbecfc0718223711c56bf287be11f3
rodrikmenezes <rodrikaraujo@gmail.com>
2019-07-30 04:07:17 -0300

    Atualizações

--- PyCursoUSP - Parte 1/semana 9/coh_piah.py
+++ PyCursoUSP - Parte 1/semana 9/coh_piah.py
@@ -3,13 +3,13 @@ import re
 def le_assinatura():
     '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
     print("Bem-vindo ao detector automático de COH-PIAH.")
-
-    wal = float(input("Entre o tamanho medio de palavra:"))
-    ttr = float(input("Entre a relação Type-Token:"))
-    hlr = float(input("Entre a Razão Hapax Legomana:"))
-    sal = float(input("Entre o tamanho médio de sentença:"))
-    sac = float(input("Entre a complexidade média da sentença:"))
-    pal = float(input("Entre o tamanho medio de frase:"))
+    print()
+    wal = float(input("Entre o tamanho medio de palavra: "))
+    ttr = float(input("Entre a relação Type-Token: "))
+    hlr = float(input("Entre a Razão Hapax Legomana: "))
+    sal = float(input("Entre o tamanho médio de sentença: "))
+    sac = float(input("Entre a complexidade média da sentença: "))
+    pal = float(input("Entre o tamanho medio de frase: "))
 
     return [wal, ttr, hlr, sal, sac, pal]
 
@@ -17,10 +17,12 @@ def le_textos():
     i = 1
     textos = []
     texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
+    print()
     while texto:
         textos.append(texto)
         i += 1
         texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
+        print()
 
     return textos
 
@@ -29,6 +31,7 @@ def separa_sentencas(texto):
     sentencas = re.split(r'[.!?]+', texto)
     if sentencas[-1] == '':
         del sentencas[-1]
+    
     return sentencas
 
 def separa_frases(sentenca):
@@ -68,12 +71,17 @@ def n_palavras_diferentes(lista_palavras):
     return len(freq)
 
 def compara_assinatura(as_a, as_b):
-    '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.
-    Esta deve ser a última a ser implementada'''
-    pass
+    '''Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas'''
+    grauSimilaridade = 0
+    for i in range(len(as_a)):
+        grauSimilaridade += abs(as_a[i]-as_b[i])
+    # grauSimilaridade = ( abs(as_a[0]-as_b[0]) + abs(as_a[1]-as_b[1]) + abs(as_a[2]-as_b[2]) + abs(as_a[3]-as_b[3]) + abs(as_a[4]-as_b[4]) + abs(as_a[5]-as_b[5]) ) / 6
+    grauSimilaridade = grauSimilaridade / 6
+
+    return grauSimilaridade
 
 def calcula_assinatura(texto):
-    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+    '''Essa funcao recebe um texto e devolve a assinatura do texto.'''
     sentencas_texto = separa_sentencas(texto) # lista com as sentenças do texto
     palavras_texto = [] # inicialização da lista com as palavras do texto
     soma_caracteres_texto = 0 # inicialização variável que soma a quantidade de caracteres de todas as palavras do texto
@@ -82,7 +90,6 @@ def calcula_assinatura(texto):
     soma_caracteres_frase = 0
 
     for sentenca in range(len(sentencas_texto)):
-        
         aux_sentenca = separa_frases(sentencas_texto[sentenca]) # separa cada sentença em frases (variável de apoio)
         soma_caracteres_sentenca += len(sentencas_texto[sentenca])
         soma_numero_frases += len(separa_frases(sentencas_texto[sentenca]))
@@ -106,15 +113,32 @@ def calcula_assinatura(texto):
     # complexidade de sentença
     sac = soma_numero_frases / len(sentencas_texto)
     # tamanho médio de frase
-    # pal = soma_caracteres_texto / len(separa_frases(texto))
-    # pal = soma_caracteres_frase / len(separa_frases(texto))
     pal = soma_caracteres_frase / soma_numero_frases
 
     return [wal, ttr, hlr, sal, sac, pal]
 
-# calcula_assinatura("Muito além, nos confins inexplorados da região mais brega da Borda Ocidental desta Galáxia, há um pequeno sol amarelo e esquecido. Girando em torno deste sol, a uma distancia de cerca de 148 milhões de quilômetros, há um planetinha verde-azulado absolutamente insignificante, cujas formas de vida, descendentes de primatas, são tão extraordinariamente primitivas que ainda acham que relógios digitais são uma grande ideia.")
-
-
 def avalia_textos(textos, ass_cp):
-    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
-    pass
+    '''Essa funcao recebe uma lista de textos e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.
+    a função compara_assinaturas é executada dentro desta função'''
+    assComparar = []
+    grauSimilaridade = [1000]
+    for i in range(len(textos)):
+        assComparar.append(calcula_assinatura(textos[i]))
+        grauSimilaridade.append(compara_assinatura(ass_cp, assComparar[i]))
+        if grauSimilaridade[-1] < grauSimilaridade[-2]:
+            menorGrau = i
+
+    return menorGrau + 1
+
+
+# inicialização do programa
+ass_cp = le_assinatura()
+print()
+# input dos textos
+textos = le_textos()
+print()
+# comparar a assinatura de cada texto presente na lista "textos" com assMestre
+cohpiah = avalia_textos(textos, ass_cp)
+# impressão do resultado final
+print('O autor do texto', str(cohpiah), 'está infectado com COH-PIAH')
+
